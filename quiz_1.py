72ef63c88c73d9993b855d6ed314e94c3ea4a1b6
jangbigom91 <jangbigom91@gmail.com>
2021-06-18 16:19:35 +0900

    0618 nadopy update

---
+++ quiz_1.py
@@ -0,0 +1,8 @@
+# Quiz1) 변수를 이용하여 다음 문장을 출력
+station = '사당'
+station_1 = '신도림'
+station_2 = '인천공항'
+
+print(station + '행 열차가 들어오고 있습니다.')
+print(station_1 + '행 열차가 들어오고 있습니다.')
+print(station_2 + '행 열차가 들어오고 있습니다.')
